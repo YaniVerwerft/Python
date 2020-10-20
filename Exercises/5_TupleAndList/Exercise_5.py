2f893240c363d4480d120fd228d36b38da4f7012
YaniVerwerft <r0856502@student.thomasmore.be>
2020-10-20 13:18:34 +0200

    ex5 Tuples

--- Exercises/5_TupleAndList/Exercise_5.py
+++ Exercises/5_TupleAndList/Exercise_5.py
@@ -1,4 +1,28 @@
-my_list = [0, 42, 18, 17, 0, 2, 19, 10, 5, 14]
+my_list = [42, 18,17, 0, 37, 0, 2, 19, 20, 5, 14,0]
 
-for i in range(len(my_list):
+print(my_list)
+
+for i in range(len(my_list)-1):
     if my_list[i] == 0:
+        largest_odd = 0
+        my_list[i] = largest_odd
+        for j in range(i+1,len(my_list)-1):
+            if my_list[j] > largest_odd and my_list[j] %2 != 0:
+                largest_odd = my_list[j]
+        my_list[i] = largest_odd
+
+print(my_list)
+
+
+# # numbers = [42, 18, 0, 37, 0, 2, 19, 20, 5, 14]
+# numbers = [42, 18, 17, 0, 2, 19, 0]
+# print(numbers)
+# for index in range(len(numbers) - 1):
+#     if numbers[index] == 0:
+# # search largest_odd
+# largest_odd = 0
+# for i in range(index + 1, len(numbers) - 1):
+#     if numbers[i] > largest_odd and numbers[i] % 2 != 0:
+#         largest_odd = numbers[i]
+# numbers[index] = largest_odd
+# print(numbers)
