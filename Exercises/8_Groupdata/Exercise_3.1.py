a1bfd20080d34eea62337fc60a3619cd358104c1
unknown <r0856502@student.thomasmore.be>
2020-11-24 15:07:17 +0100

    8.3 and 8.3.1 to contain slicing instead of more splitting

--- Exercises/8_Groupdata/Exercise_3.1.py
+++ Exercises/8_Groupdata/Exercise_3.1.py
@@ -1,3 +1,4 @@
+# this uses slicing of record 0 instead of splitting it into smaller sections.
 with open('weather_2018 10.csv', encoding='UTF-8') as file:
     for i in range(0,2):
         line = file.readline().rstrip()
@@ -7,15 +8,13 @@ with open('weather_2018 10.csv', encoding='UTF-8') as file:
     while line:
         amount_counter = 0
         total_temperature = 0
-        split_first_field = record[0].split(' ')
-        indicator = split_first_field[0]
+        indicator = record[0][0:10]
 
-        while line and indicator == split_first_field[0]:
+        while line and indicator == record[0][0:10]:
             total_temperature += float(record[1])
             amount_counter += 1
             line = file.readline().rstrip()
             record = line.split(';')
-            split_first_field = record[0].split(' ')
         average = total_temperature / amount_counter
         print(indicator, 'number of measurements = ', amount_counter,'/taverage = ',average)
     print('>' * 75)
